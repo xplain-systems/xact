8a428522a03a80180dac1b90a6341183a6793f49
William Payne <wtpayne@gmail.com>
2020-11-05 21:55:38 +0000

    FIX: Fix for Pylint W0613 Unused argument.

--- src/xact/cfg/load.py
+++ src/xact/cfg/load.py
@@ -99,7 +99,7 @@ def from_filepath(filepath_cfg):
 
 
 # -----------------------------------------------------------------------------
-def _from_xml_file(filepath_cfg, file_cfg):
+def _from_xml_file(_, file_cfg):
     """
     Return confiuguration data loaded from the specified XML file path.
 
@@ -112,7 +112,7 @@ def _from_xml_file(filepath_cfg, file_cfg):
 
 
 # -----------------------------------------------------------------------------
-def _from_json_file(filepath_cfg, file_cfg):
+def _from_json_file(_, file_cfg):
     """
     Return confiuguration data loaded from the specified JSON file path.
 
