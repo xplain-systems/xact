d3cf7191c2cfca86a1439b9860ccda33f7acbfde
William Payne <wtpayne@gmail.com>
2020-11-06 18:38:50 +0000

    FIX: Fix for Pylint C0415 Import outside toplevel.

--- src/xact/sys/__init__.py
+++ src/xact/sys/__init__.py
@@ -147,7 +147,7 @@ def _run_locally(cfg):
     and diagnosing errors.
 
     """
-    import xact.proc  # pylint: disable=W0621
+    import xact.proc  # pylint: disable=C0415,W0621
 
     id_host_local    = 'localhost'
     id_process_local = 'mainprocess'
