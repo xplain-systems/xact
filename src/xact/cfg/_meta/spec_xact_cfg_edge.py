442e67609da3210949569920dc087b7bb85220f3
William Payne <wtpayne@gmail.com>
2020-11-08 13:49:01 +0000

    FIX: Fix for PyComplexity R0968 Mccabe per comment.

--- src/xact/cfg/_meta/spec_xact_cfg_edge.py
+++ src/xact/cfg/_meta/spec_xact_cfg_edge.py
@@ -23,18 +23,11 @@ class SpecifyDenormalize:
         cfg_denorm = xact.cfg.edge.denormalize(valid_normalized_config)
 
         for cfg_edge in cfg_denorm['edge']:
-            assert 'id_edge' in cfg_edge
-            assert 'relpath_src' in cfg_edge
-            assert 'relpath_dst' in cfg_edge
-            assert 'id_node_src' in cfg_edge
-            assert 'id_node_dst' in cfg_edge
-            assert 'list_id_process' in cfg_edge
-            assert 'list_id_host' in cfg_edge
-            assert 'ipc_type' in cfg_edge
-            assert 'id_host_owner' in cfg_edge
-            assert 'id_host_src' in cfg_edge
-            assert 'id_host_dst' in cfg_edge
-            assert 'idx_edge' in cfg_edge
+            for key in ('id_edge',      'relpath_src', 'relpath_dst',
+                        'id_node_src',  'id_node_dst', 'list_id_process',
+                        'list_id_host', 'ipc_type',    'id_host_owner',
+                        'id_host_src',  'id_host_dst', 'idx_edge'):
+                assert key in cfg_edge
 
         for cfg_host in cfg_denorm['host'].values():
             assert 'is_inter_host_edge_owner' in cfg_host
